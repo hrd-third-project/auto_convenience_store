5759ae930fdf9e6deb9881ab374b956cefc9811b
330sum <126743151+330sum@users.noreply.github.com>
2023-04-13 17:57:23 +0900

    수민

--- src/Customer/CartController.java
+++ src/Customer/CartController.java
@@ -34,6 +34,8 @@ public class CartController {
     }
 
 
+
+
     // 카트에 품목을 지우는 메서드
     public static List<Item> deleteItem() {
         List<Item> cartList = CartController.getCartList();
@@ -56,7 +58,10 @@ public class CartController {
                 System.out.println(item.getItemName() + "을/를 장바구니에서 삭제하였습니다.");
             }
 
+
+
             System.out.println("\n\n \t\t ********** 수정된 장바구니 목록 **********");
+
             CartViewer.myCart();
 
         } else {
--- src/payment/Payment.java
+++ src/payment/Payment.java
@@ -1,19 +1,17 @@
 package payment;
 
-import customer.Cart;
 import customer.CartController;
 import item.Item;
-import utility.Utility;
 import view.StoreMain;
 
 import static utility.Utility.stop;
 
 public class Payment {
     //필드
-    private int payment;// 지불금액
+//    private int payment;// 지불금액
 
 
-    private static int paymentPrice; //총금액
+    private static int payment; //총금액
     private static String purchaseList = ""; // 구매리스트
 
     public static void payCheck() {
@@ -23,12 +21,12 @@ public class Payment {
             //Mycart 리스트에 담겨있는 품목 +총금액
 
             for (Item item : CartController.getCartList()) {
-                paymentPrice += item.getPrice();
+                payment += item.getPrice();
                 purchaseList += item.getItemName();
             }
 
             //Mycart 리스테 담겨있는 총금액 계산 --> 결제완료 후 처음화면으로 이동
-            System.out.println("[총금액] : " + paymentPrice);
+            System.out.println("[총금액] : " + payment);
             System.out.println("[구매목록] : " +purchaseList);
             System.out.println("감사합니다! 또 이용해주세요.");
             stop();
