fa4f4a92f2841030a54b19ebbb71cc9525b5ca64
330sum <126743151+330sum@users.noreply.github.com>
2023-04-13 16:18:48 +0900

    장바구니 삭제 기능 및 수량추가기능 완성!

--- src/view/CartViewer.java
+++ src/view/CartViewer.java
@@ -6,6 +6,8 @@ import item.Item;
 import item.ItemController;
 import utility.Utility;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,9 +19,9 @@ public class CartViewer {
 
     // 장바구니 목록 조회
     public static void myCart() {
-        System.out.println("\n\n ┌────────────────── 장바구니 목록 ──────────────────┓  ");
+        System.out.println("\n ┌────────────────── 장바구니 목록 ──────────────────┓  ");
         System.out.println(" │      상품명        단가       수량      합계       │");
-        System.out.println("\n │ ----------------------------------------------- │");
+        System.out.println(" │ ----------------------------------------------- │");
 
         List<Item> itemList = CartController.getCartList();
 
@@ -27,38 +29,39 @@ public class CartViewer {
                 .distinct()
                 .collect(Collectors.toList());
         for (Item item : distincList) {
-            System.out.println();
-            System.out.print(" \t\t" + item.getItemName() + "\t\t");
+            System.out.print(" │ \t\t" + item.getItemName() + "\t\t");
             System.out.print(item.getPrice() + "\t\t");
             System.out.print(10 - item.getNum() + "\t\t");
-            System.out.print(item.getPrice() * (10 - item.getNum()) + "\t   ");
+            System.out.println(item.getPrice() * (10 - item.getNum()) +"\t   │");
         }
 
 
-        System.out.println("\n │ ----------------------------------------------- │");
+        System.out.println(" │ ----------------------------------------------- │");
+
+        NumberFormat formatter = new DecimalFormat("#,###");
         int sum = 0;
         for (Item myCartItem : itemList) {
             sum += myCartItem.getPrice();
         }
-        System.out.println(" │\t\t\t  [ 장바구니 총 금액 : " + sum + " ] \t\t\t  │");
+        System.out.println(" │\t\t\t[ 장바구니 총 금액 : " + formatter.format(sum) +"원 ]\t\t\t│");
         System.out.println(" └─────────────────────────────────────────────────┘  ");
 
 
-        System.out.println(" # 1. 품목삭제 ");
-
-        String menuNum = Utility.input(" - 메뉴 입력: ");
-        switch (menuNum) {
+        System.out.println(" * 삭제하고자 하는 품목이 있다면 '1'을 눌러주세요.");
+        System.out.println(" * 뒤로가시려면 '0'을 눌러주세요.");
+        String delItem = Utility.input("  >> ");
+        switch (delItem) {
             case "1":
-//                CartController.deleteItem();
-                break;
-            case "2":
+                CartController.deleteItem();
                 break;
             case "0":
                 return;
             default:
-                System.out.println("메뉴를 잘못 입력했습니다.");
+                System.out.println("번호를 다시 입력해주세요.");
                 break;
+
         }
+
 
     }
 
